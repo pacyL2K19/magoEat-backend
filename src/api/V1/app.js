88304d423995dcc93c145dfe1baa66a7aafbc850
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-03 23:37:28 +0200

    First stable version of the api

---
+++ src/api/V1/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const userRoute = require('./router/users.route')
+const orderRoute = require('./router/rate.route')
+
+const app = express ()
+
+mongoose.connect('mongodb+srv://pacyL:newUser1@cluster0-ujmqc.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/auth',userRoute)
+app.use('/api/order', orderRoute)
+
+module.exports = app
