9ccae65dc821e8293f1031f911fe535489cdac02
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-05 20:05:03 +0200

    User schema prepared before to be checked by validator

--- src/api/V2/modules/users/schema.ts
+++ src/api/V2/modules/users/schema.ts
@@ -1,5 +1,6 @@
 import { IUser } from './model';
-import { Schema } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
+import { accountType } from '../common/constants'
 
 /**
  * @mongoose 
@@ -13,5 +14,29 @@ const UserSchema : Schema = new Schema ({
         required : true,
         unique : true,
         trim : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    adress : {
+        type : String,
+    },
+    phone : {
+        type : String,
+    },
+    accountType : {
+        type : accountType
+    },
+    profilePicture : {
+        type : String
+    },
+    mail : {
+        type : String
+    },
+    signedOn : {
+        type : Date
     }
 })
+
+export default mongoose.model('User', UserSchema);
