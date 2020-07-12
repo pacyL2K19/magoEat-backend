e8bf268abeb62ed0bb28a54752dbbe34db39c883
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-12 22:41:59 +0200

    create user middleware

--- src/api/V2/modules/users/service.ts
+++ src/api/V2/modules/users/service.ts
@@ -1,6 +1,19 @@
+import { statusCode } from './../common/constants';
+import { Response, Request } from 'express';
 import { IUser } from './model';
 import User from './schema'
 
 export default class UserService {
-    
+    public createUser (userParams : IUser, res : Response, req : Request, callback : any ) {
+        const user = new User (userParams)
+        user.save()
+            .then()
+            .catch((err) => {
+                res.status(statusCode.bad_request).json({
+                    success : false,
+                    message : 'Failed to create a user, something went wrong',
+                    err
+                })
+            })
+    }    
 }
