6ef0f0cc8dce596f902debb185f58e8d2082b174
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-16 15:57:17 +0200

    Updating user.controller.ts file

--- src/api/V2/modules/users/service.ts
+++ src/api/V2/modules/users/service.ts
@@ -1,23 +1,12 @@
-import { statusCode } from './../common/constants';
-import { Response, Request } from 'express';
+// import { statusCode } from './../common/constants';
+// import { Response, Request } from 'express';
 import { IUser } from './model';
 import User from './schema'
 
 export default class UserService {
-    public createUser (userParams : IUser, res : Response, req : Request, callback : any ) {
+    public createUser (userParams : IUser, callback : any ) {
         const user = new User (userParams)
-        user.save()
-            .then(user => {
-                res.status(statusCode.success).json({
-                    
-                })
-            })
-            .catch((err) => {
-                res.status(statusCode.bad_request).json({
-                    success : false,
-                    message : 'Failed to create a user, something went wrong',
-                    err
-                })
-            })
+        user.save(callback)
     }    
 }
+1
