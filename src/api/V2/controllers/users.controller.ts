7836d847fa5efb1f4c10281b537baa500ebaf992
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-16 22:49:24 +0200

    updating user.controller

--- src/api/V2/controllers/users.controller.ts
+++ src/api/V2/controllers/users.controller.ts
@@ -1,17 +1,53 @@
+import { successResponse } from './../modules/common/service';
+import { statusCode } from './../modules/common/constants';
+import { loginValidation } from './../validations/users.validation';
 import { Response, Request } from 'express';
+import User from '../modules/users/schema'
 import * as bcrypt from 'bcrypt';
 import validations from '../validations';
 import UserService from '../modules/users/service'
 
 export class UserController {
-    async login (req : Request, res : Response ) {
+    public login (req : Request, res : Response ) {
         // first, let validate data : 
-        
+        const {error, value} = loginValidation.validate(req.body)
+        if (error !== undefined)
+            res.status(statusCode.bad_request).json({
+                success : false,
+                message : `Rassurez vous d'avoir bien completer les champs requis dans le format requis`,
+                status : 'Failled'
+            })
+        // we have to check if the user exists 
+        User.findOne({ username : value.username })
+            .then((user) => {
+                if (user) {
+                    res.status(statusCode.forbiden).json({
+                        success : false,
+                        status : 'Faillure',
+                        message : `Cet utulisateur n'existe pas, avez-vous deja un compte MagoEat ?`
+                    })
+                }
+                bcrypt.compare(req.body.password, value.password)
+                    .then((isSame) => {
+                        if (!isSame) {
+                            res.status(statusCode.forbiden).json({
+                                success : false,
+                                status : 'Faillure',
+                                message : `Mot de passe incorrect`
+                            })
+                        }
+                        successResponse(res, 'Ouverture de session reussie', req.body)
+                    })
+                    .catch((err) => {
+
+                    })
+            })
+            .catch()
     }
-    async signup (req : Request, res : Response) {
+    public async signup (req : Request, res : Response) {
 
     } 
-    async receiveSmsFromAdmin () {
+    public async receiveSmsFromAdmin () {
 
     }
 }
