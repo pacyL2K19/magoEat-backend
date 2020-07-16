c93fb36d394dee50eaff878df89615164f9d2104
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-16 21:54:56 +0200

    Updating user.service file

--- src/api/V2/modules/users/service.ts
+++ src/api/V2/modules/users/service.ts
@@ -7,6 +7,19 @@ export default class UserService {
     public createUser (userParams : IUser, callback : any ) {
         const user = new User (userParams)
         user.save(callback)
-    }    
+    }
+    public findUserId(query : any, callback : any) {
+        User.findOne(query, callback)
+    }
+
+    public updateUser (userParams : IUser, callback) {
+        const query = {_id : userParams._id}
+        User.updateOne(query, userParams, callback)
+    }
+
+    public deleteUser (_id : String, callback : any) {
+        const query = { _id }
+        User.deleteOne(query, callback)
+    }
 }
 1
