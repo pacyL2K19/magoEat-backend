c4db814dbaa46fd20027ea27bd11868209c44085
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-18 23:49:20 +0200

    userRoutes for login and signup

--- src/api/V2/routes/users.routes.ts
+++ src/api/V2/routes/users.routes.ts
@@ -4,6 +4,11 @@ import { Application, Request, Response } from 'express';
 export class UserRoutes {
     private userController : UserController = new UserController ()
     public route (app : Application) {
-        app.post()
+        app.post('api/v2/login', (req : Request, res : Response) => {
+            this.userController.login(req, res)
+        })
+        app.post('api/vs/signup', (req : Request, res : Response) => {
+            this.userController.signup(req, res)
+        })
     }
 }
