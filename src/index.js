a240b91fbb1d7f887c2fe0fb6ba55f57fd8c331b
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-19 23:09:45 +0200

    Updating app file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-"use strict";
-exports.__esModule = true;
-require("dotenv/config");
-var app_1 = require("./app");
-var _a = process.env.PORT, PORT = _a === void 0 ? 5000 : _a;
-app_1["default"].listen(PORT, function () { return console.log("Server listening on Port " + PORT); });
+"use-strict"
+import app from './app'
+import env from './api/V2/environnement'
+
+const PORT = env.getPort()
+app.listen(PORT, () => {
+    console.log(`App listening port ${PORT}`)
+})
