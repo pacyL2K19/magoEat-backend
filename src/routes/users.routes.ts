0785ea22f829bd754325ebbc29b3b3d659da8c6e
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-25 20:56:15 +0200

    Changing router logic for better implementation

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,21 @@
 import { UserController } from './../controllers/users.controller';
-import { Application, Request, Response } from 'express';
+import { Application, Request, Response, Router } from 'express';
 
+/**
+ * @class for user routes
+ */
 export class UserRoutes {
     private userController : UserController = new UserController ()
-    public route (app : Application) {
-        app.post('api/v2/login', (req : Request, res : Response) => {
-            this.userController.login(req, res)
-        })
-        app.post('api/v2/signup', (req : Request, res : Response) => {
-            this.userController.signup(req, res)
-        })
+    public route (router : Router) {
+        router.route('/login')
+            .post(this.userController.login);
+        router.route('/signup')
+            .post(this.userController.signup)
+        // app.post('api/v2/login', (req : Request, res : Response) => {
+        //     this.userController.login(req, res)
+        // })
+        // app.post('api/v2/signup', (req : Request, res : Response) => {
+        //     this.userController.signup(req, res)
+        // })
     }
 }
