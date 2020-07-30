4052f6185b04e216a7b554b535c6163f76ff2c4d
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-30 14:12:55 +0200

    Minor update

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-// import { CommonRoutes } from './routes/common.routes';
+import { UserController } from './controllers/users.controller';
+import { CommonRoutes } from './routes/common.routes';
 import * as bodyParser from 'body-parser';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import * as mongoose from 'mongoose';
 import cors from 'cors';
 import helmet from 'helmet';
@@ -14,22 +15,28 @@ class App {
      * So remember to use string type instead of String wich is an object
      * As you can face tsc trouble when you compile
      */
-    public mongoUrl : string | undefined = process.env.DATABASE_URL; 
-    private userRoutes : UserRoutes = new UserRoutes ()
-    // private commonRoute : CommonRoutes = new CommonRoutes () 
+    public mongoUrl : string | undefined = process.env.DATABASE_URL;
+    private userRoutes : UserRoutes = new UserRoutes ();
+    private commonRoute : CommonRoutes = new CommonRoutes ();
+    // private userController : UserController = new UserController ();  
 
     constructor () {
         this.app = express ();
         this.configApp ();
         this,this.setupDB ();
         // this.app.use('/api/v2',route); // for all routes avoiding maany useless lines of code
-        // this.commonRoute.route(this.app);
         this.userRoutes.route(this.app);
+        // this.commonRoute.route(this.app);
     }
 
     private configApp () : void {
         this.app.use(bodyParser.json()); // supports json docs post 
         this.app.use(bodyParser.urlencoded({ extended : false }));
+        // this.app.get('/log', (req : Request, res : Response) => {
+        //     res.status(200).json({
+        //         message: "ok"
+        //     })
+        // })
         this.app.use(cors());
         this.app.use(helmet())
     }
